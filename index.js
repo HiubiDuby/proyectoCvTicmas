5b7936f4e1b88ba04160cd44ecef5f54a5e3d57b
Andres Arausay <andres.arausay@gmail.com>
2023-02-25 19:50:57 -0300

    Parametrizacion de animación de tarjetas

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Cierra el nav-bar cuando se hace click
 
-$('.nav-link').on('click',function() {
-    $('.navbar-collapse').collapse('hide');
+$(".nav-link").on("click",function() {
+    $(".navbar-collapse").collapse("hide");
   });
 
-  
+// Cambiar cartas
+$(".ref-button").on("click", function(){
+    $(".referencia-trabajo").toggle("swing")
+    $(".descrip-trabajo").toggle("swing")
+})
