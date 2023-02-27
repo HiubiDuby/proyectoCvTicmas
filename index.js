ead36f0cf73aaa779f3ac45c0a7b49e20dddaf05
Andres Arausay <andres.arausay@gmail.com>
2023-02-26 21:33:15 -0300

    Refactoring y comentarios

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ $(".nav-link").on("click",function() {
     $(".navbar-collapse").collapse("hide");
   });
 
-// Cambiar cartas
+// Modificar cartas
 
 $(".ref-button, .desc-button").on("click", function(){
-  var footerSelected = $(this).parent()
+  //Cambia el texto del boton
+  var footerSelected = $(this).parent();
   footerSelected.children(".ref-button").toggle();
   footerSelected.children(".desc-button").toggle();
+  // Cambia el texto dentro de la carta seleccionada
   var cardSelected =  $(this).parent().prev();
   cardSelected.children(".referencia-trabajo").toggle("swing");
   cardSelected.children(".descrip-trabajo").toggle("swing");
